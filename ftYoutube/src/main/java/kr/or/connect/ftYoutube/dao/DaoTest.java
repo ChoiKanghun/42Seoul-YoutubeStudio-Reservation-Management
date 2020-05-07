b1228434695dab63ff05931a3824c86c1f49ace1
ChoiKanghun <rkdgns4562@naver.com>
2020-05-07 10:43:35 +0900

    Test: test make columns moved to the left

--- ftYoutube/src/main/java/kr/or/connect/ftYoutube/dao/DaoTest.java
+++ ftYoutube/src/main/java/kr/or/connect/ftYoutube/dao/DaoTest.java
@@ -10,18 +10,11 @@ public class DaoTest {
 	public static void main(String[] args) {
 		System.out.println("hi I'm DAO TEST");
 		ApplicationContext ac = new AnnotationConfigApplicationContext(ApplicationConfig.class);
-		StudioDao studioDao = ac.getBean(StudioDao.class);
 		System.out.println("hi Im ac getBean");
 
-		for (int i = 1; i <= 168; i++) {
-			if (i % 7 == 0) {
-				System.out.println(i);
-			} else {
-				studioDao.studioDayMove(1, i, i + 1);
-			}
-		}
-		int remove1 = studioDao.removeFirstDay(1);
-		System.out.println("hi Im remove1 : " + remove1);
-	}
+		
+		StudioDao studioDao = ac.getBean(StudioDao.class);
+
+
 
 }
