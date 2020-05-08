631799a796565f588a796346e288079201000bbc
ChoiKanghun <rkdgns4562@naver.com>
2020-05-08 16:34:51 +0900

    Delete: deleted all system.out.println

--- ftYoutube/src/main/java/kr/or/connect/ftYoutube/service/ScheduleService.java
+++ ftYoutube/src/main/java/kr/or/connect/ftYoutube/service/ScheduleService.java
@@ -20,25 +20,24 @@ public class ScheduleService {
 	@Scheduled(cron="00 00 00 * * ?")
 	public void testMethod() 
 	{
-
-		for (int i = 1; i <= 168; i++) 
-		{
-			if (i % 7 == 0) 
-				System.out.print("삭제한 레코드의 id : " + i+ ", ");
-			else 
-				studioDao.studioDayMove(1, i, i + 1);
-		}
-		int remove1 = studioDao.removeFirstDay(1);
-		System.out.println("hi I'm remove1 count : " + remove1);
+		int temp = 0;
 		
 		for (int i = 1; i <= 168; i++) 
 		{
 			if (i % 7 == 0) 
-				System.out.print("삭제한 레코드의 id : " + i);
+				temp++;
+			else 
+				studioDao.studioDayMove(1, i, i + 1);
+		}
+		studioDao.removeFirstDay(1);
+		
+		for (int i = 1; i <= 168; i++) 
+		{
+			if (i % 7 == 0) 
+				temp++;
 			else 
 				studioDao.studioDayMove(2, i, i + 1);
 		}
-		int remove2 = studioDao.removeFirstDay(2);
-		System.out.println("hi I'm remove2 count : " + remove2);
+		studioDao.removeFirstDay(2);
 	}
 }
